cbaa2c065d083915efc895e81d815a7aa974e254
nicolysampaio <78435150+nicolysampaio@users.noreply.github.com>
2021-12-13 20:21:32 -0300

    learning react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ListaDeNotas } from "./components/ListaDeNotas";
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota" />
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas />
+    </section>
+  );
+}
+
+export default App;
